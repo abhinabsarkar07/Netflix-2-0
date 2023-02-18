706893fe4e0123e5e1be0a8c10c9fe10505ed37f
Abhinab Sarkar <abhinab@trustedcommunities.org>
2023-02-18 16:31:44 +0530

    created different rows for the movies

--- src/Features/LargeRow/Row.js
+++ src/Features/LargeRow/Row.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from './../Axios';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import "./Row.css";
@@ -7,29 +7,48 @@ function Row({title, fetchUrl, isLargeRow = false}) {
   
   const[root, setRoot] = useState([]);
 
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
       setRoot(request.data.results);
       return request;
     }
+
     fetchData();
+
   }, [fetchUrl]);
 
 
-console.log(root);
+    console.log(root);
 
 
+    return (
+      <div className="row">
+      <h2>{title}</h2>
 
-  return (
+        <div className= "row_posters">
 
-    <div className="row">
-        <h2>{title}</h2>
+          {root.map((movie) => (
+            ((isLargeRow && movie.poster_path) ||
+            (!isLargeRow && movie.backdrop_path)) && (
+            <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name} 
+            />
+          )
+          ))}
 
-
-
-    </div>
-  )
+        </div>
+      </div>
+    
+    
+      );
 }
 
-export default Row
+export default Row;
